0a800736e45de3047bf6c1c6288cf75376c2a369
Julius Adetya <juliusadetya@gmail.com>
2024-06-23 12:09:12 +0700

    Initial Commit

---
+++ Terra-Peregrini-Rahasya/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Terra-Peregrini-Rahasya
+//
+//  Created by Julius Adetya on 23/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Terra-Peregrini-Rahasya/Terra_Peregrini_RahasyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Terra_Peregrini_RahasyaApp.swift
+//  Terra-Peregrini-Rahasya
+//
+//  Created by Julius Adetya on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Terra_Peregrini_RahasyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
