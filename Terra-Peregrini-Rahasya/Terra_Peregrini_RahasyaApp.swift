7bf3f7173c193d639d0e6434808f8dd711e11e69
Julius Adetya <juliusadetya@gmail.com>
2024-07-08 22:23:22 +0700

    [UPDATE] New AppIcon

--- Terra-Peregrini-Rahasya/Terra_Peregrini_RahasyaApp.swift
+++ Terra-Peregrini-Rahasya/Terra_Peregrini_RahasyaApp.swift
@@ -15,7 +15,7 @@ struct Terra_Peregrini_RahasyaApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationStack(path: $router.navPath) {
-                MissionIntro(mission: 3)
+                DisclaimerView()
                     .navigationDestination(for: Router.Destination.self) { destination in
                         switch destination {
                         case .minimumreq:
